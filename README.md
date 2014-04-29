8e55927cd4aceb1bca6933ed3ea63bed5d8acbfb
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-04-29 13:46:09 -0400

    added xmldom parser

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ domdom
 ======
 
 Quick test of xml parsers for node.js
+
+
+```
+npm install
+node index.js
+```
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var fs = require("fs");
 var jsdom =require("jsdom");
 var libxmljs = require("libxmljs");
 
+var xmldom = require("xmldom");
+
 // from http://ejohn.org/blog/javascript-benchmark-quality/
 function runTest(name, test, next){
   var runs = [], r = 0;
@@ -52,6 +54,15 @@ console.timeEnd("jsdom")
 
 //console.log(doc2);
 
+
+console.time("xmldom");
+var p = new xmldom.DOMParser()
+var doc3 = p.parseFromString(xml, 'text/xml');
+console.timeEnd("xmldom")
+
+//console.log(doc2);
+
+
 function test1(){
     var doc1 = libxmljs.parseXml(xml);
 }
@@ -60,20 +71,28 @@ function test2(){
     var doc2 = jsdom.jsdom(xml, jsdom.level(1, "core"));
 }
 
+function test3(){
+    var doc3 = new xmldom.DOMParser().parseFromString(xml, 'text/xml');
+}
+
 function done(name, runs){
     console.log("test: "+name+ ", runs: "+runs);
 }
 
 runTest("jsdom", test2);
 runTest("libxmljs", test1);
+runTest("xmldom", test3);
 
 /*
 
 Sample output:
 
-libxmljs: 3ms
-jsdom: 472ms
-test: jsdom, runs: 14,13,14,14,14
-test: libxmljs, runs: 710,668,722,633,696
+libxmljs: 8ms
+jsdom: 664ms
+xmldom: 86ms
+
+test: jsdom, runs: 11,12,12,12,12
+test: libxmljs, runs: 578,585,603,603,603
+test: xmldom, runs: 80,96,109,108,114
 
 */
